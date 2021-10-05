9502f43ee5b039cf82944dd71b48728c809ab7e7
Hugo Carbiener <41008899+Hugo-Carbiener@users.noreply.github.com>
2021-10-05 17:31:50 +0200

    Add homepage

---
+++ agvoy-app/src/Controller/Controller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Room;
+use App\Form\RoomType;
+use App\Repository\RoomRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class Controller extends AbstractController
+{
+    /**
+     * @Route("", name="homepage", methods={"GET"})
+     * @Route("/home", name="homepage", methods={"GET"})
+     */
+    public function homepage(RoomRepository $roomRepository): Response
+    {
+        return $this->render('home.html.twig');
+    }
+}
