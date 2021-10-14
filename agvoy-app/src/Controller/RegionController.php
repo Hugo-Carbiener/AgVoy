b6df66b534b8da9c34de4431f98588b71c9d1318
Hugo Carbiener <41008899+Hugo-Carbiener@users.noreply.github.com>
2021-10-15 00:35:47 +0200

    Remove backoffice form region routes

--- agvoy-app/src/Controller/RegionController.php
+++ agvoy-app/src/Controller/RegionController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/backoffice/region")
+ * @Route("/region")
  */
 class RegionController extends AbstractController
 {
